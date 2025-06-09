c59a71718a994b4c285d3a7ba6fab2be5a6e1955
nuhajoy <nuhaminemnebered@gmail.com>
2025-06-09 23:52:58 +0300

    Routes

---
+++ Server/routes/answerRoute.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const authMiddleware = require("../middleware/authMiddleware");
+
+const {
+  singleQuestion,
+  postAnswer,
+} = require("../controller/answerController");
+// user controller
+
+router.get("", singleQuestion);
+
+router.post("", authMiddleware, postAnswer);
+
+module.exports = router;
