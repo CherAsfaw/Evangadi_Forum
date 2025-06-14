44c6ce95b43e76074c5b3df0e32cfa7769e47cba
Chernet <cherrefera@gmail.com>
2025-06-14 05:31:55 +0300

    add to envitomental variable

--- Server/db/dbConfig.js
+++ Server/db/dbConfig.js
@@ -1,11 +1,12 @@
 const mysql2 = require("mysql2");
+require('dotenv').config();
 
 const dbConnection = mysql2.createPool({
-  host: "localhost",
-  user: "evangadi-admin",
-  password: "123456",
-  database: "evangadi_forum",
-  connectionLimit: 10,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT) || 10,
 });
 
 module.exports = dbConnection.promise();
